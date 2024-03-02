8f7b2275fe5fbeaa9cdacd234cdd052eaad56fa8
James Best <jim@justjibba.net>
2024-03-02 05:35:55 +0000

    Create post router

--- social_api/main.py
+++ social_api/main.py
@@ -1,22 +1,7 @@
 from fastapi import FastAPI
-
-from social_api.models.post import UserPost, UserPostIn
+from social_api.routers.post import router as post_router
 
 app = FastAPI()
 
-
-post_table = {}
-
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.dict()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+# app.include_router(post_router, prefix="posts")
+app.include_router(post_router)
