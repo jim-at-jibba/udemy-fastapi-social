629cfde9c2fef2d4bb3b493540b80e5ffb8e42d1
James Best <jim@justjibba.net>
2024-03-21 07:12:42 +0000

    Set up multi env configutation

--- social_api/config.py
+++ social_api/config.py
@@ -3,9 +3,27 @@ from typing import Optional
 
 
 class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 class GlobalConfig(BaseConfig):
     DATABASE_URL: Optional[str] = None
     DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    # dev configuration will be prefixed with DEV_
+    # pydantic will strip out the DEV_ prefix and just use the variable name
+    model_config = SettingsConfigDict(env_prefix="DEV_")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_")
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: Optional[str] = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    model_config = SettingsConfigDict(env_prefix="TEST_")
