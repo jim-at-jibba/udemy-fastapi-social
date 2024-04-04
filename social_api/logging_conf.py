9b6a2e8d77743d1914dd1cae39e51a877f413699
James Best <jim@justjibba.net>
2024-04-04 08:50:59 +0100

    Add basic logging configuration

---
+++ social_api/logging_conf.py
@@ -0,0 +1,33 @@
+from logging.config import dictConfig
+
+from social_api.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(name)s:%(lineno)d - %(message)s",
+                }
+            },
+            "handlers": {
+                "default": {
+                    "class": "logging.StreamHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                }
+            },
+            "loggers": {
+                "social_api": {
+                    "handlers": ["default"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                }
+            },
+        }
+    )
--- social_api/main.py
+++ social_api/main.py
@@ -1,11 +1,17 @@
+import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from social_api.routers.post import router as post_router
 from social_api.database import database
+from social_api.logging_conf import configure_logging
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("Yo")
     await database.connect()
     yield
     # this is the cleanup code
