b6aa466ca8f79a5eda61b6ce7e36f8c6a602706c
James Best <jim@justjibba.net>
2024-04-05 05:39:56 +0100

    Add file based logging

--- social_api/logging_conf.py
+++ social_api/logging_conf.py
@@ -13,7 +13,12 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
@@ -21,15 +26,24 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "social_api.log",
+                    "maxBytes": 1024 * 1024,
+                    "backupCount": 2,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
                 "uvicorn": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "INFO",
                 },
                 "social_api": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 },
