1e5607ab4ea80a0aed4a3939846b307be7e5ef87
James Best <jim@justjibba.net>
2024-04-05 08:51:56 +0100

    Add email obfuscation filter

--- social_api/logging_conf.py
+++ social_api/logging_conf.py
@@ -1,6 +1,28 @@
+import logging
 from logging.config import dictConfig
 
 from social_api.config import DevConfig, config
+
+
+def obfuscated(email: str, obfuscated_length: int) -> str:
+    characters = email[:obfuscated_length]
+    first, last = email.split("@")
+    return characters + ("*" * (len(first) - obfuscated_length)) + "@" + last
+
+
+class EmailObfuscationFilter(logging.Filter):
+    def __init__(
+        self,
+        obfuscated_length: int,
+        name: str = "",
+    ) -> None:
+        super().__init__(name)
+        self.obfuscated_length = obfuscated_length
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        if "email" in record.__dict__:
+            record.email = obfuscated(record.email, self.obfuscated_length)
+        return True
 
 
 def configure_logging() -> None:
@@ -15,7 +37,11 @@ def configure_logging() -> None:
                     "()": "asgi_correlation_id.CorrelationIdFilter",
                     "uuid_length": 8 if isinstance(config, DevConfig) else 32,
                     "default_value": "-",
-                }
+                },
+                "email_obfuscation": {
+                    "()": EmailObfuscationFilter,
+                    "obfuscated_length": 2 if isinstance(config, DevConfig) else 0,
+                },
             },
             "formatters": {
                 "console": {
@@ -35,7 +61,7 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
                 "rotating_file": {
                     "class": "logging.handlers.RotatingFileHandler",
@@ -45,7 +71,7 @@ def configure_logging() -> None:
                     "maxBytes": 1024 * 1024,
                     "backupCount": 2,
                     "encoding": "utf8",
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
             },
             "loggers": {
